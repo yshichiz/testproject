6f134c1d0b471c07d54bcf3a2d668b7507eaeeb7
yshichiz <67945522+yshichiz@users.noreply.github.com>
2020-08-19 22:02:22 +0900

    Add files via upload

---
+++ hopsrun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hops -Xmx650G --input /data2/yshichiz/Downloads/test_data/*fastq --output /data2/yshichiz/Downloads/hopsoutput -m full -c /home/yshichiz/miniconda3/envs/hops/share/hops1configFile.txt
+
